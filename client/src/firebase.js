871986e5b6a14b381cc82e83ca67705d0d5cdd9e
Timothy Attah <timothyattah733@gmail.com>
2021-10-04 18:43:14 +0100

    Initial commit

---
+++ client/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyDyMKtmfmZ_NP7jPbwXaeWTKDYMOuk5oPI',
+	authDomain: 'e-clone-43de8.firebaseapp.com',
+	projectId: 'e-clone-43de8',
+	storageBucket: 'e-clone-43de8.appspot.com',
+	messagingSenderId: '593765011274',
+	appId: '1:593765011274:web:cf055ea4007b1852533bb3',
+	measurementId: 'G-XJRVW5PBPD',
+};
