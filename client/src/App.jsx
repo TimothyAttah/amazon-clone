5b65dd24b68421ee45c1ea499d8a6e2cf6932895
Timothy Attah <timothyattah733@gmail.com>
2021-10-05 00:38:32 +0100

    Added react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Header } from './components/header/Header'
 import { Home } from './home/Home';
 
 export const App = () => {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
-  )
+		<Router>
+			<div className='app'>
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+          </Route>
+          <Route path='/' exact>
+            <Header />
+            <Home />
+          </Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
