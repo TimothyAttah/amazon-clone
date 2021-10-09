1707aeb0625099af31bb4feeec09105a03a002c3
Timothy Attah <timothyattah733@gmail.com>
2021-10-09 11:55:27 +0100

    Created and added new images folder and images.js file

---
+++ client/src/components/images.js
@@ -0,0 +1,27 @@
+import Logo from '../images/img-1.png';
+import BackgroundImg from '../images/img-3.jpg';
+import Item1 from '../images/img-2.jpg';
+import Item2 from '../images/img-4.jpg';
+import Item3 from '../images/img-5.jpg';
+import Item4 from '../images/img-6.jpg';
+import Item5 from '../images/img-7.jpg';
+import Item6 from '../images/img-8.jpg';
+import Item7 from '../images/img-9.jpg';
+import Item8 from '../images/img-10.jpg';
+import Item9 from '../images/img-11.jpg';
+import Item10 from '../images/img-12.jpg';
+
+export const images = {
+  Logo,
+  BackgroundImg,
+  Item1,
+  Item2,
+  Item3,
+  Item4,
+  Item5,
+  Item6,
+  Item7,
+  Item8,
+  Item9,
+  Item10,
+}
