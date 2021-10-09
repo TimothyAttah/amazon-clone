c7d5e884e695064bf845090bdea09458a82040cd
Timothy Attah <timothyattah733@gmail.com>
2021-10-09 20:48:32 +0100

    Set up firebase user authentication

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
 	apiKey: 'AIzaSyDyMKtmfmZ_NP7jPbwXaeWTKDYMOuk5oPI',
 	authDomain: 'e-clone-43de8.firebaseapp.com',
@@ -7,3 +9,10 @@ const firebaseConfig = {
 	appId: '1:593765011274:web:cf055ea4007b1852533bb3',
 	measurementId: 'G-XJRVW5PBPD',
 };
+
+const firebaseApp = firebase.initializeApp( firebaseConfig );
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth(); 
+
+export { db, auth };
--- client/src/login/Login.jsx
+++ client/src/login/Login.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { images } from '../components/images';
 import './Login.css';
 
 export const Login = () => {
+  const [ email, setEmail ] = useState( '' );
+  const [ password, setPassword ] = useState( '' );
+  const signIn = ( e ) => {
+    e.preventDefault();
+  }
+  const register = ( e ) => {
+    e.preventDefault();
+  }
   return (
     <div className="login">
       <Link to='/'>
@@ -17,11 +25,11 @@ export const Login = () => {
         <h1>Sign-in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
           <h5>Password</h5>
-          <input type="password" />
-          <button className="login__signInButton">Sign In</button>
+          <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button type="submit" onClick={signIn} className="login__signInButton">Sign In</button>
         </form>
         <p>
           By signing-in you agree to AMAZON FAKE CLONE
@@ -29,7 +37,7 @@ export const Login = () => {
           Please see our Privacy Notice, our Cookies Notice and
           our Interest-Based Ads Notice.
         </p>
-        <button className="login__registerButton">Create your Amazon Account</button>
+        <button type="submit" onClick={register} className="login__registerButton">Create your Amazon Account</button>
       </div>
     </div>
   )
