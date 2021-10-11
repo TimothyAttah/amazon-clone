b597e4ad69bbebb76a830d95edc37de6f9d75f76
Timothy Attah <timothyattah733@gmail.com>
2021-10-11 12:35:26 +0100

    Added links to orders in header file

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -34,15 +34,21 @@ export const Header = () => {
 			</div>
 			<div className='header__nav'>
 				<Link to={!user && '/login'}>
-				<div onClick={handleAuthentication} className='header__option'>
-					<span className='header__optionLineOne'>Hello {user ? user[0]?.email : 'Guest'}</span>
-					<span className='header__optionLineTwo'>{user ? 'Sign Out' : 'Sign In'}</span>
-				</div>
+					<div onClick={handleAuthentication} className='header__option'>
+						<span className='header__optionLineOne'>
+							Hello {user ? user[0]?.email : 'Guest'}
+						</span>
+						<span className='header__optionLineTwo'>
+							{user ? 'Sign Out' : 'Sign In'}
+						</span>
+					</div>
 				</Link>
-				<div className='header__option'>
-					<span className='header__optionLineOne'>Returns</span>
-					<span className='header__optionLineTwo'>& orders</span>
-				</div>
+				<Link to="/orders">
+					<div className='header__option'>
+						<span className='header__optionLineOne'>Returns</span>
+						<span className='header__optionLineTwo'>& orders</span>
+					</div>
+				</Link>
 				<div className='header__option'>
 					<span className='header__optionLineOne'>Your</span>
 					<span className='header__optionLineTwo'>Prime</span>
@@ -50,7 +56,9 @@ export const Header = () => {
 				<Link to='/checkout'>
 					<div className='header__optionBasket'>
 						<ShoppingBasket />
-						<span className='header__optionLineTwo header__basketCount'>{ basket?.length }</span>
+						<span className='header__optionLineTwo header__basketCount'>
+							{basket?.length}
+						</span>
 					</div>
 				</Link>
 			</div>
